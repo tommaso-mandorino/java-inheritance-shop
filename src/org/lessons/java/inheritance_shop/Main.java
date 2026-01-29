b8cc528e7068201562a30627aa3880d343979395
tommaso-mandorino <tommasomandorino@outlook.it>
2026-01-29 16:36:50 +0100

    feat: add Product class with its attributes and methods

---
+++ src/org/lessons/java/inheritance_shop/Main.java
@@ -0,0 +1,29 @@
+package org.lessons.java.inheritance_shop;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Product product1 = new Product(3000, "Product 1 Name", "Product 1 Brand", new BigDecimal("100.99"), new BigDecimal("22"));
+
+        System.out.println(product1.getCode());
+        System.out.println(product1.getName());
+        System.out.println(product1.getBrand());
+        System.out.println(product1.getPrice());
+        System.out.println(product1.getVat());
+
+        product1.setName("Product 1 changed name");
+        product1.setBrand("Product 1 changed brand");
+        product1.setPrice(new BigDecimal("50"));
+        product1.setVat(new BigDecimal("30"));
+
+        System.out.println();
+
+        System.out.println(product1.getCode());
+        System.out.println(product1.getName());
+        System.out.println(product1.getBrand());
+        System.out.println(product1.getPrice());
+        System.out.println(product1.getVat());
+    }
+}
---
+++ src/org/lessons/java/inheritance_shop/Product.java
@@ -0,0 +1,53 @@
+package org.lessons.java.inheritance_shop;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Product {
+    
+    private int code;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private BigDecimal vat;
+
+    public Product(int code, String name, String brand, BigDecimal price, BigDecimal vat) {
+        this.code = code;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.vat = vat;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price.setScale(2, RoundingMode.HALF_EVEN);
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getVat() {
+        return this.vat;
+    }
+    public void setVat(BigDecimal vat) {
+        this.vat = vat;
+    }
+}
